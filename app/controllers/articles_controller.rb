3e5d77518bf5d45326f0558560d8518cd7380169
Thamer Aissaoui <thameur.aissaoui@gmail.com>
2018-08-16 02:03:45 +0000

    CRUD done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    def edit
+         @article = Article.find(params[:id])
+    end
+    
     def create
     @article = Article.new(params.require(:article).permit(:title, :text))
     if  @article.save
@@ -24,6 +28,25 @@ class ArticlesController < ApplicationController
     
     end
     end
+    
+  def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+  end
+  
+  
+  
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
     
     private
     
